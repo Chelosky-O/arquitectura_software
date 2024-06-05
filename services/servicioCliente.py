84ce17a2ea636022f64ff269a46329ec7245d97c
ignvcio42 <ignacio.guajardo_o_m@mail.udp.cl>
2024-06-05 15:49:40 -0400

    creacion de cliente y servicio de los usuarios/clientes

---
+++ services/servicioCliente.py
@@ -0,0 +1,103 @@
+import socket
+import mysql.connector
+from mysql.connector import Error
+
+def connect_db():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            database='CyberCafeManager',
+            user='user',
+            password='user_password'
+        )
+        return connection
+    except Error as e:
+        print(f"Error: {e}")
+        return None
+
+def handle_request(servicio, datos):
+    response = ""
+    db_connection = connect_db()
+    if db_connection is None:
+        return f"{servicio}NKFailed to connect to database"
+
+    cursor = db_connection.cursor()
+
+    if servicio == "CODAU":  # Añadir usuario
+        dataArray = datos.split('-')
+        query = "INSERT INTO Cliente (rut, nombre, email) VALUES (%s, %s, %s)"
+        cursor.execute(query, (dataArray[0], dataArray[1], dataArray[2]))
+        db_connection.commit()
+        response = f"{servicio}OKUser added with RUT {dataArray[0]}"
+
+    elif servicio == "CODEL":  # Eliminar usuario
+        query = "DELETE FROM Cliente WHERE rut = %s"
+        cursor.execute(query, (datos,))
+        db_connection.commit()
+        response = f"{servicio}OKUser with RUT {datos} deleted"
+
+    elif servicio == "CODMU":  # Modificar usuario
+        dataArray = datos.split('-')
+        query = "UPDATE Cliente SET nombre = %s, email = %s WHERE rut = %s"
+        cursor.execute(query, (dataArray[1], dataArray[2], dataArray[0]))
+        db_connection.commit()
+        response = f"{servicio}OKUser with RUT {dataArray[0]} updated"
+
+    elif servicio == "CODGU":  # Obtener información del usuario
+        query = "SELECT rut, nombre, email FROM Cliente WHERE rut = %s"
+        cursor.execute(query, (datos,))
+        result = cursor.fetchone()
+        if result:
+            response = f"{servicio}OK{result[0]}-{result[1]}-{result[2]}"
+        else:
+            response = f"{servicio}NKNo user found with RUT {datos}"
+
+    db_connection.close()
+    return response
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+bus_address = ('localhost', 5000)
+print('starting up on {} port {}'.format(*bus_address))
+sock.bind(bus_address)
+sock.listen(1)
+
+try:
+    while True:
+        print('waiting for a connection')
+        client_socket, client_address = sock.accept()
+        try:
+            print('connection from', client_address)
+
+            while True:
+                amount_received = 0
+                amount_expected = int(client_socket.recv(5))
+                data = b''
+                while amount_received < amount_expected:
+                    chunk = client_socket.recv(amount_expected - amount_received)
+                    if not chunk:
+                        break
+                    data += chunk
+                    amount_received += len(chunk)
+                
+                if not data:
+                    break
+                
+                print("Processing ...")
+                print('received {!r}'.format(data))
+
+                servicio = data[:5].decode()
+                datos = data[5:].decode()
+                print(f"Service: {servicio}, Data: {datos}")
+
+                response = handle_request(servicio, datos)
+
+                response_length = len(response)
+                message = f"{response_length:05}{response}".encode()
+                print('sending {!r}'.format(message))
+                client_socket.sendall(message)
+        finally:
+            client_socket.close()
+finally:
+    print('closing socket')
+    sock.close()
